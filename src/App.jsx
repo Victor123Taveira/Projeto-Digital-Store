35152631d2aaebe9c4f21e64c3f5f86f9fabe763
Victor123Taveira <victor.tav@hotmail.com>
2025-05-11 11:27:14 -0300

    Subindo projeto para o GitHub

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ProductListingPage from './pages/ProductListingPage';
+import ProductViewPage from './pages/ProductViewPage';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import './App.css';
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/produtos" element={<ProductListingPage />} />
+        <Route path="/produto/:id" element={<ProductViewPage />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
